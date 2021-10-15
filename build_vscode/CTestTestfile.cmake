f12d5fb5bd76499db3315cd3765b4bbe159ea7c1
AndrevvRoman <andrey_romanof@mail.ru>
2021-10-15 12:59:24 +0700

    added gitignore

--- build_vscode/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/romanov_as/codeorient-test-case
-# Build directory: /home/romanov_as/codeorient-test-case/build_vscode
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libs/args")
